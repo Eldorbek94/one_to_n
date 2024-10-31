9d3ac443ea748c6a69cd0a75d09c01238e257291
Eldorbek94 <eldorbtc@gmail.com>
2024-10-31 12:12:09 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Programm Calculate one to N!");
+Console.WriteLine("Enter a positive integer (N): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int summ = (n * (n + 1)) / 2;
+
+Console.WriteLine($"The sum from 1 to {n} is: {summ}");
